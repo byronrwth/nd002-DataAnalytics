384783ce461845e1c414ab5e975fe174c65ca60b
xi cheng <byroneagle@hotmail.com>
2017-01-06 21:42:47 +0100

    add ipynb for naive bayse and svm comparision

--- P5_fraud/choose_your_own/your_algorithm.py
+++ P5_fraud/choose_your_own/your_algorithm.py
@@ -31,14 +31,46 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+# You will need to complete this function imported from the ClassifyNB script.
+# Be sure to change to that code tab to complete this quiz.
 
+######### Naive Bayes ##############
+'''  
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+print clf.score(features_test, labels_test)
+### draw the decision boundary with the text points overlaid
 
+'''
 
+######### SVM ##############
+'''
+from time import time
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVC(kernel = "rbf", max_iter = 10000000, C = 100000000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" # train 2.55 s
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"  # predict 0.334 s
+
+###
+#training time: 3.342 s
+#predict time: 0.002 s
+#Accuracy: 0.96
+##
+'''
 
 
 try:
     prettyPicture(clf, features_test, labels_test)
+    print "Accuracy: " + str(accuracy_score(pred,labels_test))
 except NameError:
     pass
