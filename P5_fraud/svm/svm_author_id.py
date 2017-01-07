d4264bd3c745df7c77cfd6725af1ba2c70983ddd
xi cheng <byroneagle@hotmail.com>
2017-01-07 20:46:01 +0100

    compare classifying algorithms of naive bayes and SVM with different kernels

--- P5_fraud/svm/svm_author_id.py
+++ P5_fraud/svm/svm_author_id.py
@@ -24,7 +24,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from time import time
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+#clf = svm.SVC(kernel = "rbf", max_iter = 10000000, C = 100000000)
 
+clf = svm.SVC(kernel = 'rbf', C=10000.0)
+
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" # train 2.55 s
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"  # predict 0.334 s
+
+
+pred = clf.predict(features_test)
+
+print accuracy_score(pred, labels_test)
+
+print len(pred), sum(pred)
 #########################################################
 
 
